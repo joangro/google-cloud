c0bf6d55aa7e4bbcd3651896c3d9a444e3609929
joangro <jgraunoel@gmail.com>
2019-02-07 16:39:58 +0100

    added python custom logs

---
+++ stackdriver-logging/custom-logs-python/main.py
@@ -0,0 +1,15 @@
+from google.cloud import logging
+from google.cloud.logging.resource import Resou
+log_client = logging.Client()
+log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions' # Example creates a log under cloud functions resource
+res = Resource(type="cloud_function", 
+               labels={
+                   "function_name": "CLOUD-FUNCTION-NAME", 
+                   "region": "CLOUD-FUNCTION-LOCATION"
+               },
+              )
+logger = log_client.logger(log_name.format("MY-PROJECT-NAME"))
+logger.log_struct(
+ {"message": "hi"}, resource=res, severity='ERROR')
+
+return 'Wrote logs to {}.'.format(logger.name)
